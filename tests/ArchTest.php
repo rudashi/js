cdc5641b929c87147f8009df47ece1f4634183f2
Borys Żmuda <rudashi@gmail.com>
2024-09-04 22:32:24 +0200

    feat: Added new architecture tests

--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -5,5 +5,18 @@ declare(strict_types=1);
 arch()->preset()->php();
 
 arch('strict types')
-    ->expect('src')
+    ->expect('Rudashi\JavaScript')
     ->toUseStrictTypes();
+
+arch('closed for extension')
+    ->expect('Rudashi\JavaScript')
+    ->classes()
+    ->not->toBeAbstract()
+    ->toBeReadonly()
+    ->toExtendNothing()
+    ->toBeFinal();
+
+arch('annotations')
+    ->expect('Rudashi\JavaScript')
+    ->toHavePropertiesDocumented()
+    ->toHaveMethodsDocumented();
